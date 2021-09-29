9597935da7fb62528cca331748b854309d6265e3
vikash <vikashyadav20072002@gmail.com>
2021-09-29 12:12:54 +0530

    trying

---
+++ android/app/src/main/kotlin/com/example/music_playet/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.music_playet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+void main(){
+  runApp(MaterialApp(
+home: Scaffold(appBar:AppBar(
+  title:const Text('Mift',
+
+  textAlign: TextAlign.end,
+  ),backgroundColor:CupertinoColors.darkBackgroundGray
+),
+  body:const Center(child: Text('Handsome Munda'),
+
+   ) ,
+
+),
+  ));
+}
